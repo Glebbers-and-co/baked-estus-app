ce32b3c7818e4f5a340ec52acaada94de7b9f54a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-16 11:03:11 +0300

    🚮 remove design system component from main page

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,6 @@ export default function Home() {
   return (
     <main className={cn(styles.mainPage)}>
       <UiContainer>index page</UiContainer>
-
-      <DesignSystem />
     </main>
   );
 }
