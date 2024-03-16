bc591e02111b95720995ec418dbbc215f631c9a0
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-16 15:40:34 +0300

    🗑️ remove design system from exports

--- src/components/ui/kit/index.ts
+++ src/components/ui/kit/index.ts
@@ -1,7 +1,6 @@
 import Checkbox from './Checkbox/Checkbox';
-import DesignSystem from './DesignSystem/DesignSystem';
 import InlineLink from './InlineLink/InlineLink';
 import ListItem from './lists/ListItem/ListItem';
 
-export { DesignSystem, ListItem, InlineLink, Checkbox };
+export { ListItem, InlineLink, Checkbox };
 export * from './lists/List/List';
