4d6d56573b49d4ea77b6f66bf65dcd8d4e27219c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-16 11:29:12 +0300

    🔽 add paddings to header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,14 +1,24 @@
 import cn from 'classnames';
 import { type FC } from 'react';
 
+import { AppConstants } from '@/app/app.constants';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 
+import styles from './Header.module.scss';
 import type { HeaderProps } from './Header.props';
 
 const Header: FC<HeaderProps> = () => {
   return (
-    <header className={cn('sticky top-0 bg-primary-bg with-shadow')}>
-      <UiContainer>Header</UiContainer>
+    <header
+      className={cn('sticky top-0 bg-primary-bg with-shadow', styles.appHeader)}
+    >
+      <UiContainer
+        maxWidth={AppConstants.containersWidths.wide}
+        className={cn(styles.container)}
+        as={'div'}
+      >
+        <div>Header</div>
+      </UiContainer>
     </header>
   );
 };
